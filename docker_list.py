9b68af369b13eb1eeecca03e7fb0c7d75a35b688
Sam Collins <sam.collins@influentialsoftware.com>
2016-01-15 13:18:00 +0000

    Using glade partials

--- docker_list.py
+++ docker_list.py
@@ -11,7 +11,12 @@ import time
 from io import StringIO, BytesIO
 import subprocess
 from lxml.html import parse
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk, GLib, Gdk, GdkPixbuf
+
+import gdocker_logs
 
 import dbus
 import dbus.service
@@ -23,7 +28,7 @@ DBusGMainLoop(set_as_default=True)
 bus = dbus.SessionBus()
 container_proxy = bus.get_object('org.freedesktop.container', '/org/freedesktop/container')
 container_iface = dbus.Interface(container_proxy, dbus_interface='org.freedesktop.container')
-
+app_name = "Docker"
 
 from docker import Client
 docker_client = Client(base_url='unix://var/run/docker.sock')
@@ -52,7 +57,10 @@ def get_container_forwards(container_info):
     
     localhost = '127.0.0.1'
     dockerhost = container_info['NetworkSettings']['IPAddress']
-    
+
+    if not container_info['NetworkSettings']['Ports']:
+        return
+
     for port in container_info['NetworkSettings']['Ports'].keys():
         ports = ''.join([l for l in port if l.isdigit()])
         if container_info['NetworkSettings']['Ports'].get(port):
@@ -100,6 +108,9 @@ class application_gui:
         self.openFetcher()
         self.refresh()
 
+        # l = gdocker_logs.LogWindow()
+
+
     def openFetcher(self):
         self.window.show_all()
 
@@ -115,14 +126,8 @@ class application_gui:
 
         for container in get_containers(filter=filter_string):
             container_info = docker_client.inspect_container(container['Id'])
-            link = None
-            ports = [port for port in get_container_forwards(container_info)]
-            if ports:
-                print link
-                link = ports[0]
-            
             #~ menu_container = gtk.MenuItem(container_info['Name'])
-            self.listbox.model_append(container, '', container_info['Name'], 'test', link)
+            self.listbox.list_containers(container, container_info)
 
     def closeFetcher(self, widget):
         self.window.hide()
@@ -135,70 +140,56 @@ class ListBoxSelect:
     def __init__(self, listbox):
         """ pass in list box to manage and connect event"""
         self.listbox = listbox
-        self.listbox.connect('row-activated', self.selection)
+        #self.listbox.connect('row-activated', self.selection)
         container_proxy.connect_to_signal("state_change", self.update, dbus_interface="org.freedesktop.container")
 
-    def selection(self, lbox, lbrow):
-        """ row selected we may want to react here"""
-        container_id = lbrow.get_name()
-        boxrow = lbrow.get_children()[0]
-        boxinfo = boxrow.get_children()[1]
-        print(boxinfo.get_children()[1].get_text())
+    def selection(self, listbox, container, container_info):
+        """ docker container has been selected so open terminal """
+        print 'called'
+        if container_info['State']['Running']:
+            subprocess.call(
+                ['gnome-terminal', '--tab', '-e', '''/bin/bash -c "echo 'Connecting to container: %s';
+                sudo docker exec -it %s /bin/bash"''' % (container_info['Name'][1:], container_info['Id'])]
+            )
+        else:
+            print "Container not started"  # todo: display error message
 
-        print get_container_name(container_id)
-        container_name = get_container_name(container_id)
-        subprocess.call(
-            ['gnome-terminal', '--tab', '-e', '''/bin/bash -c "echo 'Connecting to conatiner named %s';sudo docker exec -it xe_container /bin/bash"''' % (container_name,container_name)])
-        print 'background'
 
-    def model_append(self, container, image, title, description, link):
-        """ create new widgets, and connect events for our new row"""
+    def list_containers(self, container, container_info):
+        """ fill list with all docker containers """
         items = {}
-        items['row'] = Gtk.ListBoxRow()
-        items['row'].set_name(container.get('Id'))
-        items['vbox'] = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        
-        items['label1'] = Gtk.Label('%s [%s]' % (title, container.get('Status')), xalign=0)
-        items['vbox'].pack_start(items['label1'], True, False, 0)
-        
-        if link:
-            items['label2'] = Gtk.LinkButton(uri=link, label=link, xalign=0)
-            items['vbox'].pack_start(items['label2'], True, False, 0)
-        #~ items['progress'] = Gtk.ProgressBar()
-        #~ items['progress'].hide()
-        #~ items['progress'].set_fraction(0) 
+        glade_row = Gtk.Builder()
+        glade_row.add_from_file('label.glade')
 
-        #~ items['vbox'].pack_start(items['label2'], True, False, 0)
-        #~ items['vbox'].pack_start(items['progress'], False, False, 0)
+        items['row'] = glade_row.get_object("containerRow")
+        items['row'].set_name(container_info['Id'])
+        items['row'].connect('activate', self.selection, container, container_info)
 
-        items['hbox'] = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        items['image'] = Gtk.Image.new_from_icon_name('computer-fail' if self.is_container_running(container) else 'computer', 48)
-        items['switch'] = Gtk.Switch()#label="Start/Stop"
-        items['switch'].set_active(False if 'Exited' in container.get('Status') else True)
+        items['title'] = glade_row.get_object("dockerTitle")
+        items['title'].set_label('%s - %s' % (container_info['Name'][1:], container.get('Status')))
+
+        items['image'] = glade_row.get_object("dockerStatus")
+        if container_info['State']['Paused']:
+            items['image'].set_from_icon_name("gtk-media-pause", 10)  # todo: find nice image
+        elif container_info['State']['Running']:
+            items['image'].set_from_icon_name("gtk-yes", 64)
+
+        items['switch'] = glade_row.get_object("dockerToggle")
         items['switch'].connect('state-set', self.container_toggle_status, container)
+        if container_info['State']['Running']:
+            items['switch'].set_active(True)
 
-        items['button'] = Gtk.Button(label='Remove')
+        items['button'] = glade_row.get_object("dockerRemove")
         items['button'].connect('button_press_event', self.container_delete, items['row'], container)
-        #~ items['button'].set_fraction(0) 
-
-        items['hbox'].pack_start(items['image'], False, False, 0)
-        items['hbox'].pack_start(items['vbox'], True, True, 0)
-        items['button_box'] = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-
-        items['button_box'].pack_end(items['button'], False, False, 0)
-        items['button_box'].pack_end(items['switch'], False, False, 0)
-        items['hbox'].pack_start(items['button_box'], False, False, 0)
-        items['row'].add(items['hbox'])
 
         self.listbox.add(items['row'])
         items['row'].show_all()
-
         self.gui_rows[container['Id']] = items
 
     def is_container_running(self, container):
-        if 'Exited' in container.get('Status'):
-            return True
-        return False
+        if 'Running' in container.get('Status'):
+            return False
+        return True
 
     def update(self, container_id):
         print 'container daemon state change'
@@ -209,21 +200,21 @@ class ListBoxSelect:
         #~ self.gui_rows[container_id]['switch'].set_state(state)
 
     def container_state_change(self, container_id):
-        
+
         print 'async callback'
         print container_id
         state = container_iface.container_status(container_id)
         print state
 
         #~ if state != self.gui_rows[container_id]['switch'].get_active():
-            #~ self.gui_rows[container_id]['switch'].set_active(state)
+        #~ self.gui_rows[container_id]['switch'].set_active(state)
         self.gui_rows[container_id]['row'].set_sensitive(True)
 
     def container_change_status(self, container, state):
         if state is False:
             print 'stopping'
             container_iface.container_stop(container.get('Id'), reply_handler=self.container_state_change, error_handler=self.container_state_change)
-            return 
+            return
         print 'starting'
         container_iface.container_start(container.get('Id'), reply_handler=self.container_state_change, error_handler=self.container_state_change)
         return False
@@ -240,14 +231,14 @@ class ListBoxSelect:
         print container
         return
 
-    #~ def download(self, widget, args, items, link):
+        #~ def download(self, widget, args, items, link):
         #~ """ download button click, change widgets and start the progress bar and download """
         #~ items['button'].hide()
         #~ items['job'] = subprocess.Popen(
-            #~ ['curl', '-O', link],
-            #~ shell=False,
-            #~ stdout=subprocess.PIPE,
-            #~ stderr=subprocess.PIPE)
+        #~ ['curl', '-O', link],
+        #~ shell=False,
+        #~ stdout=subprocess.PIPE,
+        #~ stderr=subprocess.PIPE)
 
         #~ GLib.timeout_add_seconds(1, self.update_active_progress_bar, items)
 
