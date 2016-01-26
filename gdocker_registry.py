86285b2852735250cc2139602c09fd5508deb8d0
Oliver Marks <oliver.marks@influentialsoftware.com>
2016-01-26 14:02:21 +0000

    now lists images from local and from the registry and can be filtered

--- gdocker_registry.py
+++ gdocker_registry.py
@@ -18,30 +18,42 @@ import settings
 #/v2/_catalog?n=<integer>
 
 def get_registry_images(url, version='v2', search=None):
+    print search
+    dangling_image = 0
+    if version == 'l':
+        results = docker_client.images()
+        for item in results:
+            labels = item.get('Labels')
+            if item.get('RepoTags') == ['<none>:<none>']:
+                dangling_image += 1
+                continue
+            if search in item.get('RepoTags')[0]:
+                yield {'name': item.get('RepoTags')[0]}
+    else:
 
-    final_url = '%s/%s/' % (url, version)
-    if search:
-        final_url += 'search?q=%s' % search
-    else:
-        final_url += '_catalog'
-    print final_url
-    r = requests.get(final_url)
-    registry = r.json()
-    print registry
-    
-    if search and version == 'v1':
-        for item in registry.get('results'):
+        final_url = '%s/%s/' % (url, version)
+        if search:
+            final_url += 'search?q=%s' % search
+        else:
+            final_url += '_catalog'
+        r = requests.get(final_url)
+        registry = r.json()
+        
+        #~ if search and version == 'v1':
+        for item in registry.get('results' if version=='v1' else 'repositories'):
             print item
-    else:
-        for item in registry.get('repositories'):
-            print item
+            yield item
+        #~ else:
+            #~ for item in registry.get('repositories'):
+                #~ print item
 
 class registry_browser():
     def __init__(self):
         #load in our glade interface
         xml = Gtk.Builder()
-        xml.add_from_file('gdocker_registry.glade')
+        xml.add_from_file('glade/gdocker_registry.glade')
         window = xml.get_object('window_registry')
+        self.widget_image_list = xml.get_object('lb_image_list')
         self.registry_list = xml.get_object('cmb_registrys')
         search_registry_widget = xml.get_object('entry_search')
         search_registry_widget.connect('activate', self.search_registry)
@@ -51,25 +63,30 @@ class registry_browser():
             self.registry_list.append_text(registry[0])
         self.registry_list.set_active(0)
         
+        for image in get_registry_images('localhost', version='l', search=''):
+            self.add_image(image.get('name'))
+        
         window.show_all()
 
     def search_registry(self, widget):
+        for row in self.widget_image_list.get_children():
+            row.destroy()
+        
         registry_url = self.registry_list.get_active_text()
         search_value = widget.get_text()
-        print [(reg[0], reg[1]) for reg in settings.registry_list if reg[0] == registry_url]
         result = [(reg[0], reg[1]) for reg in settings.registry_list if reg[0] == registry_url]
         if result:
             registry_url, registry_version = result[0]
-            get_registry_images(registry_url, version=registry_version, search=search_value)
+            for item in get_registry_images(registry_url, version=registry_version, search=search_value):
+                self.add_image(item.get('name'))
+            self.widget_image_list.show_all()
 
-
-
-#~ get_registry_images('https://issdocker01.influentialsoftware.com:5000', version='v2')
-#~ get_registry_images('https://registry.hub.docker.com/', version='v1', search='python')
-
+    def add_image(self, name):
+        row = Gtk.ListBoxRow()
+        label = Gtk.Label(name)
+        row.add(label)
+        self.widget_image_list.add(row)
 
 application = registry_browser()
-
-#~ GObject.mainloop.run()
 Gtk.main()
 
--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env/python3
 
 registry_list = [
+    ('localhost', 'l'),
     ('https://issdocker01.influentialsoftware.com:5000', 'v2'),
     ('https://registry.hub.docker.com/', 'v1')]
