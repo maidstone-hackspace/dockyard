c583ec9973c7acffc17136bb958c0b810c814637
Oliver Marks <oliver.marks@influentialsoftware.com>
2016-01-26 09:19:23 +0000

    Simple registry search, not displaying yet

--- gdocker_registry.py
+++ gdocker_registry.py
@@ -11,7 +11,7 @@ from dbus.mainloop.glib import DBusGMainLoop
 
 from docker import Client
 docker_client = Client(base_url='unix://var/run/docker.sock')
-
+import settings
 
 #https://registry.hub.docker.com/v1/search?q=python
 #https://issdocker01.influentialsoftware.com:5000/v2/_catalog
@@ -24,6 +24,7 @@ def get_registry_images(url, version='v2', search=None):
         final_url += 'search?q=%s' % search
     else:
         final_url += '_catalog'
+    print final_url
     r = requests.get(final_url)
     registry = r.json()
     print registry
@@ -41,12 +42,29 @@ class registry_browser():
         xml = Gtk.Builder()
         xml.add_from_file('gdocker_registry.glade')
         window = xml.get_object('window_registry')
+        self.registry_list = xml.get_object('cmb_registrys')
+        search_registry_widget = xml.get_object('entry_search')
+        search_registry_widget.connect('activate', self.search_registry)
+        
+        
+        for registry in settings.registry_list:
+            self.registry_list.append_text(registry[0])
+        self.registry_list.set_active(0)
+        
         window.show_all()
-    
+
+    def search_registry(self, widget):
+        registry_url = self.registry_list.get_active_text()
+        search_value = widget.get_text()
+        print [(reg[0], reg[1]) for reg in settings.registry_list if reg[0] == registry_url]
+        result = [(reg[0], reg[1]) for reg in settings.registry_list if reg[0] == registry_url]
+        if result:
+            registry_url, registry_version = result[0]
+            get_registry_images(registry_url, version=registry_version, search=search_value)
 
 
-get_registry_images('https://issdocker01.influentialsoftware.com:5000', version='v2')
-get_registry_images('https://registry.hub.docker.com/', version='v1', search='python')
+
+#~ get_registry_images('https://issdocker01.influentialsoftware.com:5000', version='v2')
 #~ get_registry_images('https://registry.hub.docker.com/', version='v1', search='python')
 
 
---
+++ settings.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env/python3
+
+registry_list = [
+    ('https://issdocker01.influentialsoftware.com:5000', 'v2'),
+    ('https://registry.hub.docker.com/', 'v1')]
